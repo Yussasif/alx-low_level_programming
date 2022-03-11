c167803298a4ca299ce5c26d95c3a0fdb3d867d7
yussasif <yusufowoade2@gmail.com>
2022-03-11 22:52:59 +0100

    Mandatory task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("size of a char: %zu byte(s)\n", sizeof(char));
-	printf("size of an int: %zu byte(s)\n", sizeof(int));
-	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %zu byte(s)\n", sizeof(float));
+	printf("size of a char: %zu byte(s)\n", sizeof(char), stderr);
+	printf("size of an int: %zu byte(s)\n", sizeof(int), stderr);
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int), stderr);
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int), stderr);
+	printf("size of a float: %zu byte(s)\n", sizeof(float), stderr);
 	return (0);
 }
