3e4953f86eeb062f27b40650d0095ea7a5041b8b
yussasif <yusufowoade2@gmail.com>
2022-03-12 19:09:03 +0100

    Advance task commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-*main - print 00 to 99 with no duplication of digits or combos ie no 11, no 10(01)
+*main -print 00 to 99 with no duplicate of digits or combos ie no 11, no 10(01)
 *Return: Always 0 (Success)
 */
 
